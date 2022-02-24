21e97eb04d7c675777dc3d27289d0da4a080256c
cetanhota <wayne.leutwler@gmail.com>
2022-02-24 11:40:56 -0500

    minor changes

--- system_stats-V2.py
+++ system_stats-V2.py
@@ -55,12 +55,6 @@ def parttition():
     templ = "%-17s %8s %8s %8s %5s%% %9s  %s"
     print(templ % ("Device", "Total", "Used", "Free", "Use ", "Type","Mount"))
     for part in psutil.disk_partitions(all=False):
-        #if os.name == 'nt':
-            #if 'cdrom' in part.opts or part.fstype == '':
-                # skip cd-rom drives with no disk in it; they may raise
-                # ENOENT, pop-up a Windows GUI error for a non-ready
-                # partition or just hang.
-                #continue
         usage = psutil.disk_usage(part.mountpoint)
         print(templ % (
             part.device,
