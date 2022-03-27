b3b765e23ec4607aa2e738eeed4cce5e7955655b
cetanhota <wayne.leutwler@gmail.com>
2022-03-27 10:00:31 -0400

    initial commit

---
+++ mysql-monitor.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""""
+3/14/2022
+@author: wayne
+"""""
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="192.168.1.61",
+  auth_plugin='mysql_native_password',
+  user="",
+  password=""
+)
+
+mycursor = mydb.cursor()
+version = mydb.get_server_info()
+stats = mydb.cmd_statistics()
+
+mycursor.execute("show processlist")
+
+results = mycursor.fetchall()
+
+for x in results:
+    print(x)
+
+#print (version)
+#print (stats)
+  #cnx.commit()
+
+mydb.close()
