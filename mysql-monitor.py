8c4150133a43f5aa6bc1be6d7a419968db29eeb3
cetanhota <wayne.leutwyler@gmail.com>
2022-04-19 14:54:16 -0400

    lynter fixes.

--- mysql-monitor.py
+++ mysql-monitor.py
@@ -4,22 +4,25 @@
 3/14/2022
 @author: wayne
 """""
-import mysql.connector
-from tabulate import tabulate
+
 import socket
 import time
 import os
-import psutil
 import sys
+import mysql.connector
+from tabulate import tabulate
 
-class color:
-        BOLD = '\033[1m'
-        END = '\033[0m'
-        RED = '\033[91m'
-        BLUE = '\033[94m'
-        CYAN = '\033[96m'
-        GREEN = '\033[92m'
-        YELLOW = '\033[93m'
+class Color:
+    '''
+    colors
+    '''
+    BOLD = '\033[1m'
+    END = '\033[0m'
+    RED = '\033[91m'
+    BLUE = '\033[94m'
+    CYAN = '\033[96m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
 
 menu_options = {
     1: 'Show Processlist',
@@ -35,14 +38,6 @@ server=sys.argv[1]
 user=sys.argv[2]
 password=sys.argv[3]
 
-def print_menu():
-    for key in menu_options.keys():
-        print (key, '--', menu_options[key] )
-HOST = socket.gethostname()
-
-def clear():
-    os.system('clear')
-
 mydb = mysql.connector.connect(
 host= server,
 auth_plugin='mysql_native_password',
@@ -50,85 +45,125 @@ user= user,
 password=password)
 mycursor = mydb.cursor()
 
+def print_menu():
+    '''
+    print main menu
+    '''
+    for key in menu_options.keys():
+        print (key, '--', menu_options[key] )
+HOST = socket.gethostname()
+
+def clear():
+    '''
+    clear the screen
+    '''
+    os.system('clear')
+
 def fn_processlist():
+    '''
+    show processlist
+    '''
     query = "show processlist"
     mycursor.execute(query)
     results = mycursor.fetchall()
-    field_names = [i[0] for i in mycursor.description]
-    print(tabulate(results, headers=field_names, tablefmt='fancy_grid'))
+    pl_field_names = [i[0] for i in mycursor.description]
+    print(tabulate(results, headers=pl_field_names, tablefmt='fancy_grid'))
 
 def fn_connections():
-    query = "SELECT IFNULL(usr,'All Users') user,IFNULL(hst,'All Hosts') host,COUNT(1) Connections FROM ( SELECT user usr,LEFT(host,LOCATE (':',host) - 1) hst FROM information_schema.processlist WHERE user NOT IN ('system user','root')) A GROUP BY usr,hst WITH ROLLUP;"
+    '''
+    show connections
+    '''
+    query = "SELECT IFNULL(usr,'All Users') user,IFNULL(hst,'All Hosts') \
+        host,COUNT(1) Connections FROM ( SELECT user usr,LEFT(host,LOCATE (':',host) - 1) hst \
+            FROM information_schema.processlist WHERE user NOT IN ('system user','root')) \
+                A GROUP BY usr,hst WITH ROLLUP;"
     mycursor.execute(query)
     results = mycursor.fetchall()
-    field_names = [i[0] for i in mycursor.description]
-    print(tabulate(results, headers=field_names, tablefmt='fancy_grid'))
+    fnc1_field_names = [i[0] for i in mycursor.description]
+    print(tabulate(results, headers=fnc1_field_names, tablefmt='fancy_grid'))
 
     mycursor.execute("show global status like 'threads_connected'")
     threads_connected = mycursor.fetchall()
-    field_names = [i[0] for i in mycursor.description]
     print(tabulate(threads_connected, tablefmt='fancy_grid'))
 
     mycursor.execute("show global status like 'threads_running'")
     threads_running = mycursor.fetchall()
-    field_names = [i[0] for i in mycursor.description]
     print(tabulate(threads_running, tablefmt='fancy_grid'))
 
     mycursor.execute("SHOW GLOBAL STATUS LIKE 'Connection_errors%';")
     connection_errors = mycursor.fetchall()
-    field_names = [i[0] for i in mycursor.description]
     print(tabulate(connection_errors, tablefmt='fancy_grid'))
 
     mycursor.execute("SHOW GLOBAL STATUS LIKE 'Aborted_c%'")
     connection_aborted = mycursor.fetchall()
-    field_names = [i[0] for i in mycursor.description]
     print(tabulate(connection_aborted, tablefmt='fancy_grid'))
 
 def fn_bufferpool_eff():
+    '''
+    bufferpool information
+    '''
     print()
-    mycursor.execute("select variable_value from performance_schema.global_status where variable_name = 'Innodb_buffer_pool_read_requests'")
+    mycursor.execute("select variable_value \
+        from performance_schema.global_status \
+            where variable_name = 'Innodb_buffer_pool_read_requests'")
     bpoolrr = mycursor.fetchone()
+    row1 = ''
     for row1 in bpoolrr:
         print ("Innodb_buffer_pool_read_requests:", row1)
 
-    mycursor.execute("select variable_value from performance_schema.global_status where variable_name = 'Innodb_buffer_pool_reads'")
+    mycursor.execute("select variable_value \
+        from performance_schema.global_status where variable_name = 'Innodb_buffer_pool_reads'")
     bpoolr = mycursor.fetchone()
+    row2 = ''
     for row2 in bpoolr:
         print ("Innodb_buffer_pool_reads:", row2)
 
     print()
-    print (color.YELLOW + "Only",round(int(row2)/int(row1) * 100, 2),"% of Buffer Pool reads come from disk." + color.END)
+    print (Color.YELLOW + "Only",round(int(row2)/int(row1) * 100, 2),"% of Buffer Pool reads come from disk." \
+        + Color.END)
     print()
-    mycursor.execute("select variable_value from performance_schema.global_status where variable_name = 'Innodb_buffer_pool_pages_total'")
+    mycursor.execute("select variable_value \
+        from performance_schema.global_status \
+            where variable_name = 'Innodb_buffer_pool_pages_total'")
     pool_page_total = mycursor.fetchone()
+    row3 = ''
     for row3 in pool_page_total:
         print ("Innodb_buffer_pool_pages_total:", row3)
 
-    mycursor.execute("select variable_value from performance_schema.global_status where variable_name = 'Innodb_buffer_pool_pages_free'")
+    mycursor.execute("select variable_value from performance_schema.global_status where \
+        variable_name = 'Innodb_buffer_pool_pages_free'")
     pool_page_free = mycursor.fetchone()
+    row4 = ''
     for row4 in pool_page_free:
         print ("Innodb_buffer_pool_pages_free:", row4)
     bp_free = round(int(row4), 2)
     bp_total = round(int(row3), 2)
 
-    x = (bp_total - bp_free) / bp_total
-    xy = round(x * 100, 2)
+    bp_tot_used = (bp_total - bp_free) / bp_total
+    bp_tot_used = round(bp_tot_used * 100, 2)
     print()
-    print (color.YELLOW + "Buffer Pool Utilization: ", xy, "%" + color.END)
+    print (Color.YELLOW + "Buffer Pool Utilization: ", bp_tot_used, "%" + Color.END)
 
 def fn_main_menu():
+    '''
+    main menu
+    '''
     print ('')
     input("Press Enter to return to menu.")
     clear()
 
 def fn_query():
-    mycursor.execute("select query,db,exec_count,avg_latency from sys.statement_analysis where query not like '%commit%' order by exec_count desc limit 10")
+    '''
+    query info
+    '''
+    mycursor.execute("select query,db,exec_count,avg_latency from sys.statement_analysis \
+        where query not like '%commit%' order by exec_count desc limit 10")
     exe_count = mycursor.fetchall()
-    field_names = [i[0] for i in mycursor.description]
+    fnq_field_names = [i[0] for i in mycursor.description]
     print()
-    print (color.YELLOW + 'Top 10 queries, based on execute count:' + color.END)
+    print (Color.YELLOW + 'Top 10 queries, based on execute count:' + Color.END)
     print()
-    print(tabulate(exe_count, headers=field_names, tablefmt='fancy_grid'))
+    print(tabulate(exe_count, headers=fnq_field_names, tablefmt='fancy_grid'))
 
     mycursor.execute("show global status like 'questions';")
     questions = mycursor.fetchall()
@@ -162,91 +197,99 @@ def fn_query():
 
     writes = delete + update + insert
     print()
-    print(color.YELLOW + 'Read/Write Metrics:' + color.END)
+    print(Color.YELLOW + 'Read/Write Metrics:' + Color.END)
     print()
     print ("Questions:", questions)
     print ("Selects:", select)
     print ("Writes:", writes)
 
 def fn_global_io_waits():
+    '''
+    Global IO waits
+    '''
     mycursor.execute("select * from sys.waits_global_by_latency;")
     memory_global_total = mycursor.fetchall()
-    field_names = [i[0] for i in mycursor.description]
-    print(tabulate(memory_global_total, headers=field_names, tablefmt='fancy_grid'))
+    fng_field_names = [i[0] for i in mycursor.description]
+    print(tabulate(memory_global_total, headers=fng_field_names, tablefmt='fancy_grid'))
 
 def fn_memory_usage():
+    '''
+    memory usage
+    '''
     mycursor.execute("select * from sys.memory_global_total;")
     memory_global_total = mycursor.fetchall()
-    field_names = [i[0] for i in mycursor.description]
-    print(tabulate(memory_global_total, headers=field_names, tablefmt='fancy_grid')),
+    fnm_field_names = [i[0] for i in mycursor.description]
+    print(tabulate(memory_global_total, headers=fnm_field_names, tablefmt='fancy_grid'))
 
-    mycursor.execute("select thread_id,HIGH_NUMBER_OF_BYTES_USED/1024/1024 'MB Used' from performance_schema.memory_summary_by_thread_by_event_name order by HIGH_NUMBER_OF_BYTES_USED desc limit 10")
+    mycursor.execute("select thread_id,HIGH_NUMBER_OF_BYTES_USED/1024/1024 'MB Used' from \
+        performance_schema.memory_summary_by_thread_by_event_name order by HIGH_NUMBER_OF_BYTES_USED desc limit 10")
     memory_by_thread = mycursor.fetchall()
     print()
-    print(color.YELLOW + 'Memory Used per Thread:' + color.END)
-    field_names = [i[0] for i in mycursor.description]
-    print(tabulate(memory_by_thread, headers=field_names, tablefmt='fancy_grid'))
+    print(Color.YELLOW + 'Memory Used per Thread:' + Color.END)
+    fnt_field_names = [i[0] for i in mycursor.description]
+    print(tabulate(memory_by_thread, headers=fnt_field_names, tablefmt='fancy_grid'))
     print()
-    print(color.YELLOW + 'Bufferpool Memory Breakdown:' + color.END)
-    mycursor.execute("SELECT * FROM sys.memory_global_by_current_bytes WHERE event_name LIKE 'memory/innodb/buf_buf_pool';")
+    print(Color.YELLOW + 'Bufferpool Memory Breakdown:' + Color.END)
+    mycursor.execute("SELECT * FROM sys.memory_global_by_current_bytes \
+        WHERE event_name LIKE 'memory/innodb/buf_buf_pool';")
     innodb_bufpool = mycursor.fetchall()
-    field_names = [i[0] for i in mycursor.description]
-    print(tabulate(innodb_bufpool, headers=field_names, tablefmt='fancy_grid'))
+    fnmt_field_names = [i[0] for i in mycursor.description]
+    print(tabulate(innodb_bufpool, headers=fnmt_field_names, tablefmt='fancy_grid'))
 
 if __name__=='__main__':
-    #myfunc(sys.argv)
     clear()
-    while(True):
-        print(color.YELLOW + 'MySQL Monitor' + color.END)
+    while True:
+        print(Color.YELLOW + 'MySQL Monitor' + Color.END)
         mycursor.execute("select mysql_version from sys.version;")
         version = mycursor.fetchall()
-        field_names = [i[0] for i in mycursor.description]
-        print(tabulate(version, headers=field_names, tablefmt='fancy_grid'))
+        ver_field_names = [i[0] for i in mycursor.description]
+        print(tabulate(version, headers=ver_field_names, tablefmt='fancy_grid'))
 
         print ('Python Version is:',sys.version[0:5])
         print('Date and Time:',time.asctime())
-        print(color.YELLOW + '\nChoose Option:' + color.END)
+        print(Color.YELLOW + '\nChoose Option:' + Color.END)
         print_menu()
-        option = ''
+        OPTION = ''
         try:
-            option = int(input('Enter your choice: '))
+            OPTION = int(input('Enter your choice: '))
         except:
             print('Wrong input. Please enter a number ...')
-        #Check what choice was entered and act accordingly
-        if option == 1:
+            #Check what choice was entered and act accordingly
+        if OPTION == 1:
             clear()
-            print(color.BLUE + 'Processlist:' + color.END)
+            print(Color.BLUE + 'Processlist:' + Color.END)
             fn_processlist()
             fn_main_menu()
-        elif option == 2:
-             clear()
-             print(color.BLUE + 'Connection Information:' + color.END)
-             fn_connections()
-             fn_main_menu()
-        elif option == 3:
+        elif OPTION == 2:
             clear()
-            print(color.BLUE + 'Buffer Pool Information:' + color.END)
+            print(Color.BLUE + 'Connection Information:' + Color.END)
+            fn_connections()
+            fn_main_menu()
+        elif OPTION == 3:
+            clear()
+            print(Color.BLUE + 'Buffer Pool Information:' + Color.END)
             fn_bufferpool_eff()
             fn_main_menu()
-        elif option == 4:
+        elif OPTION == 4:
             clear()
-            print(color.BLUE + 'Query Information:' + color.END)
+            print(Color.BLUE + 'Query Information:' + Color.END)
             fn_query()
             fn_main_menu()
-        elif option == 5:
+        elif OPTION == 5:
             clear()
-            print(color.BLUE + 'Memory Usage:' + color.END)
+            print(Color.BLUE + 'Memory Usage:' + Color.END)
             fn_memory_usage()
             fn_main_menu()
-        elif option == 6:
+        elif OPTION == 6:
             clear()
-            print(color.BLUE + 'Global IO Waits' + color.END)
+            print(Color.BLUE + 'Global IO Waits' + Color.END)
             fn_global_io_waits()
             fn_main_menu()
-        elif option == 0:
-             clear()
-             print('Have a Nice Day!')
-             mydb.close()
-             exit()
-        else:
-             print('Invalid option. Please enter a number between 0 and 6.')
+        elif OPTION == 0:
+            clear()
+            print('Have a Nice Day!')
+            mydb.close()
+            sys.exit()
+            break
+    else:
+        print('Invalid option. Please enter a number between 0 and 6.')
