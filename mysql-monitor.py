a837f41a718ed474ee0c13b95086c8cc1b835c8c
cetanhota <wayne.leutwyler@gmail.com>
2022-05-02 09:58:12 -0400

    new error logic

--- mysql-monitor.py
+++ mysql-monitor.py
@@ -192,12 +192,18 @@ def fn_query():
     '''
     mycursor.execute("select query,db,exec_count,avg_latency from sys.statement_analysis \
     where query not like '%commit%' order by exec_count desc limit 10")
-    exe_count = mycursor.fetchall()
-    fnq_field_names = [i[0] for i in mycursor.description]
-    print()
-    print (Color.YELLOW + 'Top 10 queries, based on execute count:' + Color.END)
-    print()
-    print(tabulate(exe_count, headers=fnq_field_names, tablefmt='fancy_grid'))
+    try:
+        exe_count = mycursor.fetchall()
+        fnq_field_names = [i[0] for i in mycursor.description]
+    except mysql.connector.Error as err:
+        print(Color.RED + "Error Code:" + Color.END, err.errno)
+        print(Color.RED + "SQLSTATE" + Color.END, err.sqlstate)
+        print(Color.RED + "Message" + Color.END, err.msg)
+    else:
+        print()
+        print (Color.YELLOW + 'Top 10 queries, based on execute count:' + Color.END)
+        print()
+        print(tabulate(exe_count, headers=fnq_field_names, tablefmt='fancy_grid'))
 
     mycursor.execute("show global status like 'questions';")
     questions = mycursor.fetchall()
