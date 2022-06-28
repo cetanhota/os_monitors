ddc16e0356d7f2aab089c14ba14cac65ea9694f0
cetanhota <wayne.leutwler@gmail.com>
2022-06-27 20:39:31 -0400

    Done

--- mysql-monitor.py
+++ mysql-monitor.py
@@ -301,16 +301,10 @@ if __name__=='__main__':
     clear()
     while True:
         print(Color.YELLOW + 'MySQL Monitor' + Color.END)
-        try:
-            mycursor.execute("select mysql_version from sys.version;")
-            version = mycursor.fetchall()
-        except mysql.connector.Error as err:
-            print(Color.RED + "Error Code:" + Color.END, err.errno)
-            print(Color.RED + "SQLSTATE:" + Color.END, err.sqlstate)
-            print(Color.RED + "Message:" + Color.END, err.msg)
-        else:
-            ver_field_names = [i[0] for i in mycursor.description]
-            print(tabulate(version, headers=ver_field_names, tablefmt='fancy_grid'))
+        mycursor.execute("select mysql_version from sys.version;")
+        version = mycursor.fetchall()
+        ver_field_names = [i[0] for i in mycursor.description]
+        print(tabulate(version, headers=ver_field_names, tablefmt='fancy_grid'))
 
         mycursor.execute("select @@hostname as 'MySQL Server';")
         server_name = mycursor.fetchall()
@@ -390,4 +384,3 @@ if __name__=='__main__':
 
 if __name__=='__nomenu__':
     fn_processlist()
-
